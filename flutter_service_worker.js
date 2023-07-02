68bd5b3ae742085f9602e571f9c9273acfd82766
madhubanpietech <madhuban@pietechsolution.com>
2023-07-02 13:10:26 +0530

    PUSH

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "74f02d96d4f0cce19b549519ea24cb47",
-"index.html": "cf5d6f2eab26625a8bb319e3a655fca9",
-"/": "cf5d6f2eab26625a8bb319e3a655fca9",
-"main.dart.js": "8cdb1db9740e827c065ca7ae635d30a2",
+"index.html": "47cacc44f507d1b044f1bf23712f8710",
+"/": "47cacc44f507d1b044f1bf23712f8710",
+"main.dart.js": "02d86b5649a0b9124abdecfc6dff9575",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
